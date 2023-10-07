16eff6e74430951812ab5ffc1a96b67a2c702546
Obosie Akioyamen <obosiea1@umbc.edu>
2023-10-06 18:05:29 -0700

    Cleaning up cookie cutter + making data set read python runable

---
+++ src/logistic_regression/build_features.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import copy
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.linear_model import LogisticRegression
+
